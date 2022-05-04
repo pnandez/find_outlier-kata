f586f59321197b91f0eea82b05a980a57657a629
pnandez <paquiherca@gmail.com>
2022-05-04 12:45:53 +0100

    Passed all tests and accepted in codewars

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-console.log('Hello world!')
+export function findOutlier(integers: number[]): number {
+  const isPartyEven = integers.filter(x => x % 2 === 0).length > 1;
+  if (isPartyEven) {
+    return integers.find((x) => x % 2 !== 0)!;
+  }
+  return integers.find((x) => x % 2 === 0)!;
+}
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,6 +1,20 @@
-describe('Basic tests', () => {
-    it('Always green', () => {
-      expect(true).toBe(true)
-    });
+/* eslint-disable object-curly-spacing */
+import { findOutlier } from "../src/main";
+
+/**
+ * Test cases
+ * findOutlier([0, 1, 2]) => 1
+ * findOutlier([1, 2, 3]) => 2
+ * findOutlier([2, 6, 8, 10, 3]) => 3
+ * findOutlier([0]) => 0
+ */
+
+describe("Simple tests", () => {
+  it("should return the different number", () => {
+    expect(findOutlier([0, 1, 2])).toBe(1);
+    expect(findOutlier([1, 2, 3])).toBe(2);
+    expect(findOutlier([2, 6, 8, 10, 3])).toBe(3);
+    expect(findOutlier([0])).toBe(0);
   });
-  
+});
+
